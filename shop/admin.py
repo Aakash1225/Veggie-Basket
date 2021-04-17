d042dade84d777afd0d592e10a971c827ac84a58
Aakash1225 <74096672+Aakash1225@users.noreply.github.com>
2021-04-17 13:06:02 +0530

    Add files via upload

---
+++ shop/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Addproducts)
+# admin.site.register((Addproducts,AddComments))
+admin.site.register(Orders)
+admin.site.register(OrderUpdate)
+admin.site.register(Contact)
